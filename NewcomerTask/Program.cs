c85a29e91051f48f940f16d30a9fd792a8eeb4b0
DudkoMatt <github.dudko.matt@mail.ru>
2020-08-16 22:39:34 +0300

    chore: Init commit

---
+++ NewcomerTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewcomerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

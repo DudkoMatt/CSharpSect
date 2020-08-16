c95679a57dd183b951a6e656843efb77193b4d94
DudkoMatt <github.dudko.matt@mail.ru>
2020-08-16 23:24:42 +0300

    feat: Added new class Task

--- NewcomerTask/Program.cs
+++ NewcomerTask/Program.cs
@@ -2,6 +2,22 @@
 
 namespace NewcomerTask
 {
+    public class Task
+    {
+        private static ulong _nextTaskId = 1000;
+        
+        public string Info { get; set; }
+        public bool Completed { get; set; }
+        public ulong Id { get; }
+
+        public Task(string info)
+        {
+            Id = _nextTaskId++;
+            Info = info;
+            Completed = false;
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)

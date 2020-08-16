1337223970e0a1439bc7cd1a9308b8f0be5b24c4
DudkoMatt <github.dudko.matt@mail.ru>
2020-08-17 02:33:10 +0300

    feat: Added ConsoleHandler

--- NewcomerTask/Program.cs
+++ NewcomerTask/Program.cs
@@ -88,12 +88,103 @@ namespace NewcomerTask
                 Console.WriteLine($" {task.Id, -5}|   {(task.Completed ? "x" : " ")}   | {task.Info}");
         }
     }
+
+    public static class ConsoleHandler
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("TaskHandler");
+            Console.WriteLine();
+            Console.WriteLine("Available commands:");
+            Console.WriteLine("/add task-info");
+            Console.WriteLine("/all");
+            Console.WriteLine("/delete id");
+            Console.WriteLine("/save file-name.txt");
+            Console.WriteLine("/load file-name.txt");
+            Console.WriteLine("/complete id");
+            Console.WriteLine("/completed");
+            Console.WriteLine("/help");
+            Console.WriteLine("/exit");
+        }
+        public static void Run(ref TaskHandler taskHandler)
+        {
+            Hello();
+            var running = true;
+            while (running)
+            {
+                Console.Write("\n> ");
+                var line = Console.ReadLine();
+                if (line == null) return;
+                var args = line.Split();
+                var command = args[0];
+
+                switch (command)
+                {
+                    case "/add":
+                        if (args.Length > 1)
+                            taskHandler.AddNewTask(line.Substring(line.IndexOf(' ') + 1));
+                        else
+                            Console.WriteLine("There should be description");
+                        break;
+                    case "/all":
+                        taskHandler.PrintAllTasks();
+                        break;
+                    case "/delete":
+                        if (args.Length > 1)
+                            if (ulong.TryParse(args[1], out var res))
+                                taskHandler.DeleteTask(res);
+                            else
+                                Console.WriteLine("Cannot convert id");
+                        else
+                            Console.WriteLine("There should be id");
+                        break;
+                    case "/save":
+                        if (args.Length > 1)
+                            taskHandler.SaveToFile(line.Substring(line.IndexOf(' ')));
+                        else
+                            Console.WriteLine("There should be a name of file");
+                        break;
+                    case "/load":
+                        if (args.Length > 1)
+                            if (File.Exists(line.Substring(line.IndexOf(' '))))
+                                taskHandler = TaskHandler.LoadFromFile(line.Substring(line.IndexOf(' ')));
+                            else
+                                Console.WriteLine("File doesn't exists");
+                        else
+                            Console.WriteLine("There should be a name of file");
+                        break;
+                    case "/complete":
+                        if (args.Length > 1)
+                            if (ulong.TryParse(args[1], out var res))
+                                taskHandler.MarkCompleted(res);
+                            else
+                                Console.WriteLine("Cannot convert id");
+                        else
+                            Console.WriteLine("There should be id");
+                        break;
+                    case "/completed":
+                        taskHandler.PrintCompleted();
+                        break;
+                    case "/exit":
+                        running = false;
+                        break;
+                    case "/help":
+                        Hello();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong command. Type /help for help");
+                        break;
+                }
+            }
+        }
+    }
     
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var taskHandler = new TaskHandler();
+            ConsoleHandler.Run(ref taskHandler);
         }
     }
 }
